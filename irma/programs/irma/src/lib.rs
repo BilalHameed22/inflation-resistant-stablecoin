1975a00a626c5d5864a46b2636b4c86e5b14c37d
ctapang <carlos.tapang@rockstable.io>
2025-07-23 08:09:21 -0700

    This commit doesn't work because of incompatibilities between anchor 0.27.0 used in OpenBook V2 and anchor 0.31.0.

--- irma/programs/irma/src/lib.rs
+++ irma/programs/irma/src/lib.rs
@@ -9,18 +9,6 @@
 //     Pod,
 // };
 
-use anchor_lang::prelude::AccountInfo;
-use anchor_lang::prelude::AccountLoader;
-use anchor_lang::prelude::Context;
-use anchor_lang::prelude::CpiContext;
-use anchor_lang::prelude::msg;
-use anchor_lang::prelude::Program;
-use anchor_lang::prelude::Pubkey;
-use anchor_lang::prelude::Rent;
-use anchor_lang::prelude::Signer;
-use anchor_lang::prelude::System;
-
-use anchor_lang::prelude::*;
 
 use anchor_lang::{
     account,
@@ -40,13 +28,33 @@ use anchor_lang::{
 };
 // use anchor_lang::system_program::ID;
 
+use anchor_lang::solana_program::{
+    account_info::AccountInfo,
+    clock::Clock,
+    // CpiContext,
+    // Context,
+    entrypoint::ProgramResult,
+    msg,
+    program::{invoke, invoke_signed},
+    pubkey::Pubkey,
+    program_error::ProgramError,
+    system_instruction,
+    sysvar::{self, Sysvar},
+};
 
-pub mod iopenbook;
+use anchor_lang::prelude::*;
+
+
 pub mod pricing;
 
 
-use iopenbook::{EventHeap, Market, ConsumeEvents, EventHeapHeader, EventNode, AnyEvent, OracleConfig};
-use iopenbook::{/*OpenBookV2,*/ get_latest_slot, consume_given_events, MAX_NUM_EVENTS};
+// CPI context and consume_given_events for OpenBook V2
+use openbook_v2::ID as OPENBOOKV2_ID;
+use openbook_v2::cpi::accounts::ConsumeGivenEvents;
+use openbook_v2::cpi::consume_given_events;
+use openbook_v2::state::{EventHeap, /* EventNode, AnyEvent, */ Market};
+// use openbook_v2::state::EventHeap::MAX_NUM_EVENTS;
+
 use pricing::{
     mint_irma,
     redeem_irma,
@@ -73,8 +81,6 @@ use pricing::{
 //     }
 // }
 
-// CPI context and consume_given_events for OpenBook V2
-// use anchor_lang::prelude::{AccountInfo, CpiContext, Signer, AccountLoader, Program, Pubkey, AnchorDeserialize, AnchorSerialize};
 pub const IRMA_ID: Pubkey = pubkey!("8zs1JbqxqLcCXzBrkMCXyY2wgSW8uk8nxYuMFEfUMQa6");
 declare_id!("8zs1JbqxqLcCXzBrkMCXyY2wgSW8uk8nxYuMFEfUMQa6");
 
@@ -124,202 +130,85 @@ pub mod irma {
 
     /// Crank the OpenBook V2 from client.
     /// This function is called periodically (at least once per slot) to process events and update the IRMA state.
-    pub fn crank(ctx: Context<Crk>) -> Result<()> {
+    pub fn crank(ctx: Context<CrankAccounts>) -> Result<()> {
         crank_market(ctx)
     }
 }
 
 /// CHECK: following declares unsafe crank_market function - see comments above.
-fn crank_market(ctx: Context<Crk>) -> Result<()> {
-    let state = ctx.accounts.state.load_mut()?;
-    let slots = get_latest_slot()?;
+fn crank_market(ctx: Context<CrankAccounts>) -> Result<()> {
+    let openbook = ctx.accounts.openbook_v2.to_account_info();
+    let state = ctx.accounts.state.to_account_info(); // load_mut()?;
+    // let slots = openbook.clock.slot;
 
-    msg!("Cranking IRMA with pubkey: {:?}", state.pubkey);
+    // msg!("Cranking IRMA with pubkey: {:?}", state.pubkey);
 
 
     // let lamports: &mut u64 = Box::leak(Box::new(state.lamports));
     // let signer_account_info: &AccountInfo = &ctx.accounts.signer.to_account_info();
     // let system_program: &AccountInfo = &ctx.accounts.system_program.to_account_info();
 
-    let lamports: &mut u64 = Box::leak(Box::new(state.lamports));
-    let dummy_info = AccountInfo::new(
-        &IRMA_ID,
-        false,
-        false,
-        lamports,
-        &mut [],
-        &ctx.accounts.system_program.key,
-        false,
-        0,
-    );
+    let lamports: &mut u64 = &mut state.lamports.borrow_mut(); // Box::leak(Box::new(state.lamports));
 
-    fn alloc_heap() -> EventHeap {
-        let heap = EventHeap {
-            header: EventHeapHeader {
-                free_head: 0u16,
-                used_head: 0u16,
-                count: 0u16,
-                _padd: 0u16,
-                seq_num: 0u64,
-            },
-            nodes: [EventNode {
-                next: 0u16,
-                prev: 0u16,
-                _pad: [0u8; 4],
-                event: AnyEvent {
-                    event_type: 0u8, // Placeholder for event type
-                    padding: [0u8; 143], // Placeholder for event data
-                },
-            }; MAX_NUM_EVENTS as usize],
-            reserved: [0u8; 64],
-        };
-        return heap;
-    }
+    let openbook_id: Pubkey = Pubkey::new_from_array(OPENBOOKV2_ID.to_bytes());
 
-    // CHECK: following serializes typed object into a buffer.
-    let event_heap: EventHeap = alloc_heap();
+    // fn alloc_heap() -> EventHeap {
+    //     let heap = EventHeap {
+    //         header: /* EventHeapHeader */{ SomeStruct {
+    //             free_head: 0u16,
+    //             used_head: 0u16,
+    //             count: 0u16,
+    //             _padd: 0u16,
+    //             seq_num: 0u64,
+    //         } },
+    //         nodes: [/* EventNode */ {SomeStruct {
+    //             next: 0u16,
+    //             prev: 0u16,
+    //             _pad: [0u8; 4],
+    //             event: /* AnyEvent */ { SomeStruct {
+    //                 event_type: 0u8, // Placeholder for event type
+    //                 padding: [0u8; 143], // Placeholder for event data
+    //             } },
+    //         } }; /* MAX_NUM_EVENTS */ 600 as usize],
+    //         reserved: [0u8; 64],
+    //     };
+    //     return heap;
+    // }
+
+    // // CHECK: following serializes typed object into a buffer.
+    // let event_heap: EventHeap = alloc_heap();
+
     let mut event_heap_buffer: Vec<u8> = Vec::with_capacity(std::mem::size_of::<EventHeap>());
-    event_heap.try_serialize(&mut event_heap_buffer).unwrap();
+    // event_heap.try_serialize(&mut event_heap_buffer).unwrap();
     let boxed_heap: &'static mut Vec<u8> = Box::leak(Box::new(event_heap_buffer));
 
-    let program_id: &'static Pubkey = &IRMA_ID;
-    let events_acct: Pubkey = Pubkey::find_program_address(&[b"eventheap".as_ref()], program_id).0;
+    let events_acct: Pubkey = Pubkey::find_program_address(&[b"eventheap".as_ref()], &openbook_id).0;
     let events_key: &'static mut Pubkey = Box::leak(Box::new(events_acct));
     let lamports: &'static mut u64 = Box::leak(Box::new(100000u64));
 
     msg!("Events account key: {:?}", events_acct);
 
-    let events_info: AccountInfo<'_> = AccountInfo::new(
+    let events_info: AccountInfo = AccountInfo::new(
         events_key,
         false,
         false,
         lamports,
         boxed_heap,
-        program_id, // owner
+        &openbook_id, // owner
         false,
         0,
     );
 
     let signer_info: &AccountInfo<'_> = Box::leak(Box::new(ctx.accounts.signer.to_account_info()));
     let sys_program: &AccountInfo<'_> = Box::leak(Box::new(ctx.accounts.system_program.to_account_info()));
-
-    // fn alloc_mkt(ekey: Pubkey) -> Market {
-    //     let market = Market {
-    //         // PDA bump
-    //         bump: 0u8,
-    //         pad1: [0u8; 7],
-    //         // Number of decimals used for the base token.
-    //         //
-    //         // Used to convert the oracle's price into a native/native price.
-    //         base_decimals: 0u8,
-    //         pad2: [0u8; 7],
-    //         quote_decimals: 0u8,
-    //         pad3: [0u8; 7],
-    //         // padding1: [0u8; 5],
-
-    //         // Pda for signing vault txs
-    //         market_authority: Pubkey::new_unique(),
-
-    //         // No expiry = 0. Market will expire and no trading allowed after time_expiry
-    //         time_expiry: 0i64,
-
-    //         // Admin who can collect fees from the market
-    //         collect_fee_admin: Pubkey::new_unique(),
-    //         // Admin who must sign off on all order creations
-    //         open_orders_admin: Pubkey::new_unique(), // NonZeroPubkeyOption,
-    //         // Admin who must sign off on all event consumptions
-    //         consume_events_admin: Pubkey::new_unique(), // NonZeroPubkeyOption,
-    //         // Admin who can set market expired, prune orders and close the market
-    //         close_market_admin: Pubkey::new_unique(), // NonZeroPubkeyOption,
-
-    //         // Name. Trailing zero bytes are ignored.
-    //         name: [0u8; 16],
-
-    //         // Address of the BookSide account for bids
-    //         bids: Pubkey::new_unique(),
-    //         // Address of the BookSide account for asks
-    //         asks: Pubkey::new_unique(),
-    //         // Address of the EventHeap account
-    //         event_heap: ekey,
-
-    //         // Oracles account address
-    //         oracle_a: Pubkey::new_unique(), // NonZeroPubkeyOption,
-    //         oracle_b: Pubkey::new_unique(), // NonZeroPubkeyOption,
-    //         // Oracle configuration
-    //         oracle_config: OracleConfig {
-    //             conf_filter: 0f64,
-    //             max_staleness_slots: 0i64,
-    //             reserved: [0u8; 72],
-    //         },
-    //         pad4: [0u8; 8],
-
-    //         // Number of quote native in a quote lot. Must be a power of 10.
-    //         //
-    //         // Primarily useful for increasing the tick size on the market: A lot price
-    //         // of 1 becomes a native price of quote_lot_size/base_lot_size becomes a
-    //         // ui price of quote_lot_size*base_decimals/base_lot_size/quote_decimals.
-    //         quote_lot_size: 6i64,
-
-    //         // Number of base native in a base lot. Must be a power of 10.
-    //         //
-    //         // Example: If base decimals for the underlying asset is 6, base lot size
-    //         // is 100 and and base position lots is 10_000 then base position native is
-    //         // 1_000_000 and base position ui is 1.
-    //         base_lot_size: 6i64,
-
-    //         // Total number of orders seen
-    //         seq_num: 0u64,
-
-    //         // Timestamp in seconds that the market was registered at.
-    //         registration_time: 0i64,
-
-    //         // Fees
-    //         //
-    //         // Fee (in 10^-6) when matching maker orders.
-    //         // maker_fee < 0 it means some of the taker_fees goes to the maker
-    //         // maker_fee > 0, it means no taker_fee to the maker, and maker fee goes to the referral
-    //         maker_fee: -10000i64,
-    //         // Fee (in 10^-6) for taker orders, always >= 0.
-    //         taker_fee: 12000i64,
-
-    //         // Total fees accrued in native quote
-    //         fees_accrued: 0u128,
-    //         // Total fees settled in native quote
-    //         fees_to_referrers: 0u128,
-
-    //         // Referrer rebates to be distributed
-    //         referrer_rebates_accrued: 0u64,
-
-    //         // Fees generated and available to withdraw via sweep_fees
-    //         fees_available: 0u64,
-
-    //         // Cumulative maker volume (same as taker volume) in quote native units
-    //         maker_volume: 0u128,
-
-    //         // Cumulative taker volume in quote native units due to place take orders
-    //         taker_volume_wo_oo: 0u128,
-
-    //         base_mint: Pubkey::new_from_array([0u8; 32]), //  IRMA mint
-    //         quote_mint: Pubkey::new_from_array([0u8; 32]), // Stablecoin mint
-
-    //         market_base_vault: Pubkey::new_unique(),
-    //         base_deposit_total: 100u64,
-
-    //         market_quote_vault: Pubkey::new_unique(),
-    //         quote_deposit_total: 100u64,
-
-    //         reserved: [0u8; 128],
-    //     };
-    //     return market;
-    // }
     
     // // CHECK: following serializes typed object into a buffer.
     // let market: Market = alloc_mkt(events_acct);
-    let mut market_buffer: Vec<u8> = Vec::with_capacity(1024); // std::mem::size_of::<Market>());
+    let mut market_buffer: Vec<u8> = vec![0u8; std::mem::size_of::<Market>()];
     // market.try_serialize(&mut market_buffer).unwrap();
     let boxed_market: &mut Vec<u8> = Box::leak(Box::new(market_buffer));
 
-    let market_acct: Pubkey = Pubkey::find_program_address(&[b"market".as_ref()], program_id).0;
+    let market_acct: Pubkey = Pubkey::find_program_address(&[b"market".as_ref()], &openbook_id).0;
     let market_key: &'static mut Pubkey = Box::leak(Box::new(market_acct));
     let lamports: &'static mut u64 = Box::leak(Box::new(100000u64));
 
@@ -331,22 +220,37 @@ fn crank_market(ctx: Context<Crk>) -> Result<()> {
         false,
         lamports,
         boxed_market,
-        program_id, // owner
+        &openbook_id, // owner
         false,
         0,
     );
 
+    // let accounts = vec![
+    //     ctx.accounts.signer.to_account_info(),
+    //     events_info,
+    //     market_info,
+    //     sys_program.clone(),
+    // ];
+
+    // let cpi_ctx = CpiContext::new(openbook, accounts);
+
     let this_ctx = CpiContext::new(
-        dummy_info,
-        ConsumeEvents {
-            consume_events_admin: Signer::try_from(signer_info).unwrap(),
+        openbook,
+        ConsumeGivenEvents {
+            consume_events_admin: ctx.accounts.signer.to_account_info(),
             event_heap: events_info,
             market: market_info,
-            system_program: Program::try_from(sys_program).unwrap(),
+            // system_program: Program::try_from(sys_program).unwrap(),
         },
     );
 
-    consume_given_events(this_ctx, slots)?;
+    let slot = Clock::get()?.slot;
+
+    consume_given_events(this_ctx, vec![slot])
+        .map_err(|e| {
+            msg!("Error consuming events: {:?}", e);
+            e
+        });
     Ok(())
 }
 
@@ -396,15 +300,18 @@ fn oracle_input<'info>(
 
 
 #[derive(Accounts)]
-pub struct Crk<'info> {
+pub struct CrankAccounts<'info> {
     #[account(init, space = State::LEN, payer = signer)]
-    pub state: AccountLoader<'info, State>,
+    pub state: AccountInfo<'info>,
     #[account(mut, signer)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
+    #[account(address = OPENBOOKV2_ID)]
+    pub openbook_v2: Program<'info, System>,
 }
 
-#[account(zero_copy)]
+#[account]
+#[derive(PartialEq, Debug)]
 pub struct State {
     pub pubkey: Pubkey,
     pub mint_price: f64,
@@ -412,6 +319,9 @@ pub struct State {
     pub lamports: u64,
     padding1: [u8; 7],
     pub stablecoin: u8,
+    // #[account(address = solana_program::sysvar::clock::ID)]
+    // pub clock: Clock, // &'static dyn SolanaSysvar,
+    // pub sysvar: Sysvar<Clock>,
     padding2: [u8; 7],
     pub bump: u8,
 }
